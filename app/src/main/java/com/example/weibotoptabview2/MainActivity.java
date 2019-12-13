8f919d68862e790322ad1a04c308b760e9eaf2a6
wcj98 <860920675@qq.com>
2019-12-13 13:56:01 +0800

    测试工具

--- app/src/main/java/com/example/weibotoptabview2/MainActivity.java
+++ app/src/main/java/com/example/weibotoptabview2/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.weibotoptabview2;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,10 @@ public class MainActivity extends AppCompatActivity implements WeiBoTopTabView.O
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+   //  this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final WeiBoTopTabView tabView = findViewById(R.id.tabView);
@@ -53,6 +62,31 @@ public class MainActivity extends AppCompatActivity implements WeiBoTopTabView.O
         }
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        NavigationBarStatusBar(this,hasFocus);
+    }
+
+    /**
+     * 导航栏，状态栏隐藏
+     * @param activity
+     */
+    public static void NavigationBarStatusBar(Activity activity, boolean hasFocus){
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            View decorView = activity.getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+        //activity.getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        //activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+    }
 
 }
 
